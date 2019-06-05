298fa9fe615f6b86354aafb46fc5b10f31eca22d
Marcio Lemos <GERU\marcio@ger-nt-qua-133.geru.local>
2019-06-05 14:55:28 -0300

    Initial commit

---
+++ features/pages/homePage.js
@@ -0,0 +1,8 @@
+const Selector = require('testcafe').Selector;
+
+module.exports = class HomePage {
+    constructor () {
+        this.inputSearch = Selector('.nav-search-input');
+        this.buttonSearch = Selector('.nav-search-btn');
+    }
+}
---
+++ features/step_definitions/search_item.js
@@ -0,0 +1,31 @@
+const HomePage = require('../pages/homePage.js');
+const ListPage  = require('../pages/listPage.js');
+const Selector = require('testcafe').Selector;
+
+const { defineSupportCode } = require('cucumber');
+const home = new HomePage;
+const list = new ListPage;
+
+defineSupportCode(({ Given, When, Then }) => {
+    let testController = null;
+    
+    Given('that it is on the homepage of Mercado Livre', function () {
+        return this.waitForTestController()
+        .then((controller) => {
+            testController = controller;
+            return testController.navigateTo('https://www.mercadolivre.com.br/');
+        });
+    });
+    
+    When('I search for {stringInDoubleQuotes}', function (item)  {
+        return testController
+        .typeText(home.inputSearch, item)
+        .click(home.buttonSearch);
+    });
+    
+    Then('I view items according to my search', function () {
+        return testController
+        .expect(list.labelResult).contains("Golf");
+    });
+    
+});
